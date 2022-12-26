79fc3e1beddf76282d7586f55ddb7230ffe13a94
hlin863 <zcabhli@ucl.ac.uk>
2022-12-26 11:25:00 +0000

    Update train.py
    Testing the code with self-implemented train_test_split procedure.

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+# from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -14,71 +13,92 @@ seed = 42
 # Load in the data
 df = pd.read_csv("wine_quality.csv")
 
+# Randomize the data
+df = df.sample(frac=1, random_state=seed).reset_index(drop=True)
+
 # Split into train and test sections
 y = df.pop("quality")
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
 
-#################################
-########## MODELLING ############
-#################################
+X = df
 
-# Fit a model on the train section
-regr = RandomForestRegressor(max_depth=2, random_state=seed)
-regr.fit(X_train, y_train)
+print("Y: ", y.head())
 
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
+print("X: ", X.head())
 
-# Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+# display the columns for X
+print("X columns: ", X.columns)
 
+# using indexing to select X_train as the first 80% of X
+X_train = X[:int(0.8*len(X))]
 
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
-# Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
+y_train = y[:int(0.8*len(y))]
 
-# image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
-sns.set(style="whitegrid")
+# using indexing to select X_test as the last 20% of X
+X_test = X[int(0.8*len(X)):]
 
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
+y_test = y[int(0.8*len(y)):]
 
-plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120) 
-plt.close()
+# #################################
+# ########## MODELLING ############
+# #################################
+
+# # Fit a model on the train section
+# regr = RandomForestRegressor(max_depth=2, random_state=seed)
+# regr.fit(X_train, y_train)
+
+# # Report training set score
+# train_score = regr.score(X_train, y_train) * 100
+# # Report test set score
+# test_score = regr.score(X_test, y_test) * 100
+
+# # Write scores to a file
+# with open("metrics.txt", 'w') as outfile:
+#         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+#         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
 
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
+# ##########################################
+# ##### PLOT FEATURE IMPORTANCE ############
+# ##########################################
+# # Calculate feature importance in random forest
+# importances = regr.feature_importances_
+# labels = df.columns
+# feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
+# feature_df = feature_df.sort_values(by='importance', ascending=False,)
 
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+# # image formatting
+# axis_fs = 18 #fontsize
+# title_fs = 22 #fontsize
+# sns.set(style="whitegrid")
 
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
+# ax = sns.barplot(x="importance", y="feature", data=feature_df)
+# ax.set_xlabel('Importance',fontsize = axis_fs) 
+# ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
+# ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
+# plt.tight_layout()
+# plt.savefig("feature_importance.png",dpi=120) 
+# plt.close()
 
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
+
+# ##########################################
+# ############ PLOT RESIDUALS  #############
+# ##########################################
+
+# y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
+# y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
+# res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+
+# ax = sns.scatterplot(x="true", y="pred",data=res_df)
+# ax.set_aspect('equal')
+# ax.set_xlabel('True wine quality',fontsize = axis_fs) 
+# ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+# ax.set_title('Residuals', fontsize = title_fs)
+
+# # Make it pretty- square aspect ratio
+# ax.plot([1, 10], [1, 10], 'black', linewidth=1)
+# plt.ylim((2.5,8.5))
+# plt.xlim((2.5,8.5))
+
+# plt.tight_layout()
+# plt.savefig("residuals.png",dpi=120) 
