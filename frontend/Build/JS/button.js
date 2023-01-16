40acd2036390aa077b2554fc1e4c078897b8047a
hlin863 <zcabhli@ucl.ac.uk>
2023-01-16 18:46:17 +0000

    Updated Button Colours
    Implemented the colours for the buttons on the build page.

---
+++ frontend/Build/JS/button.js
@@ -0,0 +1,11 @@
+function build_function(){
+    console.log("This is a build function.");
+}
+
+function pending_function(){
+    console.log("This is a pending function.");
+}
+
+function cancel_function(){
+    console.log("This is a cancel function.");
+}
--- frontend/Build/action_page.php
+++ frontend/Build/action_page.php
@@ -1,24 +1,32 @@
 <?php
-    if (isset($_FILES['file'])) {
-        $host = "http://192.168.56.1:8081/Build/build.html";
-        $user = "username";
-        $pass = "password";
-        $destDir = "";    
-        $workDir = "C:\Users\haoch\OneDrive\Documents\UCL\COMPSI-Yr4\Final_Year_Project\OFFICIAL-REPO\ML-Integration-OFFICIAL\frontend";
+    if(isset($_FILES["file"]) && $_FILES["file"]["error"] == 0){
+        $allowed = array("py" => "text/python");
+        $filename = $_FILES["file"]["name"];
+        $filetype = $_FILES["file"]["type"];
+        $filesize = $_FILES["file"]["size"];
 
-        $tmpName = basename($_FILES['file']['tmp_name']);
-        move_uploaded_file($_FILES['file']['tmp_name'], $workDir.$tmpName) or die("Cannot move uploaded file to working directory");
+        // Verify file extension
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
 
-        $conn = ftp_connect($host) or die ("Cannot initiate connection to host");
-        ftp_login($conn, $user, $pass) or die("Cannot login");
-        $upload = ftp_put($conn, $destDir."/".$_FILES['file']['name'], $workDir.$tmpName, FTP_BINARY);
-        if (!$upload) {
-            echo "Cannot upload\n";
-        } else {
-            echo "Upload complete\n";
+        // Verify file size - 5MB maximum
+        $maxsize = 5 * 1024 * 1024;
+        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+
+        // Verify MYME type of the file
+        if(in_array($filetype, $allowed)){
+            // Check whether file exists before uploading it
+            if(file_exists("upload/" . $filename)){
+                echo $filename . " is already exists.";
+            } else{
+                move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $filename);
+                echo "Your file was uploaded successfully.";
+            } 
+        } else{
+            echo "Error: There was a problem uploading your file. Please try again."; 
         }
-        ftp_close($conn);
-
-        unlink($workDir.$tmpName) or die("Cannot delete uploaded file from working directory -- manual deletion recommended");
+    } else{
+        echo "Error: " . $_FILES["file"]["error"];
     }
 ?>
+
