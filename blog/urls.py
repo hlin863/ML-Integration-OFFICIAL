711b5e87652d0587194c3b6dc3693131e30800d9
hlin863 <zcabhli@ucl.ac.uk>
2023-01-26 23:25:29 +0000

    Multiple Page Navigation SUCCESS

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', views.post_build, name = 'post_build'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
