4076d2ed5839dabe787ff1c7cf58d73140018106
hlin863 <zcabhli@ucl.ac.uk>
2023-01-27 17:17:46 +0000

    Updated run_work_flow function 1
    Implemented a function to print the output when the program can time the application.

--- blog/static/blog/html/js/run.js
+++ blog/static/blog/html/js/run.js
@@ -1,3 +1,17 @@
+function get_date(){
+
+    // initialise a date variable to process when the operation started.
+    var date = new Date();
+
+    return date;
+
+}
+
 function run_workflow(){
     console.log("The workflow is running...");
+
+    var start_date = get_date();
+    
+    // The workflow is running...
+    console.log("The workflow started at " + start_date);
 }
