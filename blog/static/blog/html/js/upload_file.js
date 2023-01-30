1069924e6457930f95249f59a5667995b6de4c6b
hlin863 <zcabhli@ucl.ac.uk>
2023-01-30 14:28:38 +0000

    Updated headings for some sections

--- blog/static/blog/html/js/upload_file.js
+++ blog/static/blog/html/js/upload_file.js
@@ -1,3 +1,44 @@
+function run_uploaded_file(file) {
+
+    // get the file name and size
+    var fileName = file.name;
+
+    // check the file type to see if it is Python.
+    var fileType = file.type;
+
+    // check the file size to see if it is less than 1MB.
+    var fileSize = file.size;
+
+    // check the file type to see if it is Python.
+
+    if (fileType == "text/x-python") {
+
+
+
+    }
+
+}
+
+const dropBox = document.querySelector("#dropbox");
+dropBox.addEventListener("dragenter",dragEnter,false);
+dropBox.addEventListener("dragover",dragOver,false);
+dropBox.addEventListener("drop",drop,false);
+
+function dragEnter(e){
+	e.stopPropagation();
+	e.preventDefault();
+}
+
+function dragOver(e){
+	e.stopPropagation();
+	e.preventDefault();
+}
+
+function drop(e){
+	// 当文件拖拽到dropBox区域时,可以在该事件取到files
+	const files = e.dataTransfer.files;
+}
+
 function upload_file(){
 
     // updates the file and produce a message to state that the file has been updated
@@ -24,6 +65,10 @@ function upload_file(){
             var fileContent = e.target.result;
             // document.getElementById('display-file-details').innerHTML += fileContent;
             document.getElementById('display-code-details').innerHTML += ('<pyscript>' + fileContent + '</pyscript>');
+
+            // implement a function to run the uploaded file
+            run_uploaded_file(file);
+
         }
 
         
