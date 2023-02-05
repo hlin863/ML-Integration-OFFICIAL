3656b17804683934879cf5731b0e2cfe2b88ad23
hlin863 <zcabhli@ucl.ac.uk>
2023-02-05 19:21:19 +0000

    Updated command prompt
    Updated code to execute the command prompt after uploading the file.

--- blog/static/blog/html/js/upload_file.js
+++ blog/static/blog/html/js/upload_file.js
@@ -13,6 +13,16 @@ function run_uploaded_file(file) {
 
     if (fileType == "text/x-python") {
 
+        const { exec } = require("child_process");
+
+        exec("start cmd", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        });
 
 
     }
