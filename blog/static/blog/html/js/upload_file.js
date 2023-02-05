17b642019a076e746c7f61b5b5d73dd3d89dfbe9
hlin863 <zcabhli@ucl.ac.uk>
2023-02-05 20:53:04 +0000

    Experiment with download function part 1

--- blog/static/blog/html/js/upload_file.js
+++ blog/static/blog/html/js/upload_file.js
@@ -2,6 +2,11 @@ function upload_file(){
 
     var filename = document.getElementById('inputMLFile').files[0].name; 
 
+    var file = document.getElementById('inputMLFile').files[0];  // stores the file details..
+
+    // download file onto a local document
+    
+
     console.log(filename);
 
     // var form_data = new FormData();
@@ -22,3 +27,21 @@ function upload_file(){
     // });
 
 }
+
+// function download(filename, text) {
+//     var element = document.createElement('a');
+//     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+//     element.setAttribute('download', filename);
+  
+//     element.style.display = 'none';
+//     document.body.appendChild(element);
+  
+//     element.click();
+  
+//     document.body.removeChild(element);
+// }
+
+// // Example usage
+// document.getElementById("downloadLink").addEventListener("click", function() {
+//     download("example.txt", "This is some example text to be downloaded.");
+// });
