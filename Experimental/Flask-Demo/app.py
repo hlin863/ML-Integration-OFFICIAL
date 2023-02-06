4e0d7586496f760b64a0be9cc7fed7d42de1bb08
hlin863 <zcabhli@ucl.ac.uk>
2023-02-06 18:21:13 +0000

    Implemented the app.py functions

--- Experimental/Flask-Demo/app.py
+++ Experimental/Flask-Demo/app.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, TextAreaField, validators
 
 app = Flask(__name__)
+
+class HelloForm(Form):
+    name = TextAreaField('', [validators.DataRequired()])
 
 @app.route('/')
 
 def index():
 
-    return render_template('index.html')
+    hello_form = HelloForm(request.form)
+
+    return render_template('index.html', Form=hello_form)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+
+    hello_form = HelloForm(request.form)
+
+    if request.method == 'POST' and hello_form.validate():
+
+        # if the form is valid, redirect to the hello page
+
+        name = request.form['name']
+
+        return render_template('greeting.html', name=name)
+
+    return render_template('index.html', Form=hello_form)
 
 if __name__ == '__main__':
 
