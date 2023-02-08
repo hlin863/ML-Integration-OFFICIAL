244054506b9fe2ca6c2ca9c188bb2ac8cea2cacd
hlin863 <zcabhli@ucl.ac.uk>
2023-02-08 22:20:54 +0000

    Attempt to load file with an upload button

--- blog/static/blog/html/js/editor.js
+++ blog/static/blog/html/js/editor.js
@@ -45,3 +45,28 @@ async function evaluatePython() {
 		addToOutput(err);
 	}
 }
+
+jQuery("input#upload").change(
+	function handleFileSelect(evt) {
+		let files = evt.target.files; // FileList object
+	
+		// use the 1st file from the list
+		let f = files[0];
+
+		console.log(f.name);
+		
+		let reader = new FileReader();
+	
+		// Closure to capture the file information.
+		reader.onload = (function(theFile) {
+			return function(e) {
+			  jQuery( '#code' ).val( e.target.result );
+			};
+		  })(f);
+	
+		  // Read in the image file as a data URL.
+		reader.readAsText(f);
+	
+		document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+	}
+);
