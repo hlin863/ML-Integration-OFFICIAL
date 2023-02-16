51e863ad42de4d7b9015165b8d83a80d8977f41a
hlin863 <zcabhli@ucl.ac.uk>
2023-02-16 17:29:43 +0000

    Embedded celery into app - 1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,3 +116,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery settings
+
+CELERY_TIMEZONE = "UTC"
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
