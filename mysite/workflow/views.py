a0a27e01c934165cc2387bfa64adcae3fe8ed1d6
hlin863 <zcabhli@ucl.ac.uk>
2023-02-16 00:36:24 +0000

    Page displayed message

--- mysite/workflow/views.py
+++ mysite/workflow/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+from .templates import *
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
